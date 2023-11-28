a2199e51d28d402ec094f2de02132afaa2d68eaf
Milo <huongdtq27@gmail.com>
2023-11-28 17:14:51 +0700

    integrate with api

--- controller/userController.js
+++ controller/userController.js
@@ -1,13 +1,44 @@
-const getUserList = (req, res, next) => {
-  console.log("🚀 ~ file: userController.js:3 ~ getUserList :");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const getUserList = async (req, res, next) => {
+  try {
+    const post = await prisma.user.findMany();
+    console.log("🚀 ~ file: userController.js:18 ~ getUserList ~ users:", post);
+    res.json(post);
+  } catch (error) {
+    console.log(
+      "🚀 ~ file: userController.js:10 ~ getUserList ~ error:",
+      error
+    );
+  }
 };
 
 const getUserById = (req, res, next) => {
   console.log("🚀 ~ file: userController.js:12 ~ getUserById :");
 };
 
-const createUser = (req, res, next) => {
-  console.log("🚀 ~ file: userController.js:18 ~ createUser :");
+const createUser = async (req, res, next) => {
+  console.log("🚀 ~ file: userController.js:23 ~ createUser ~ req:", req.body);
+  try {
+    //create
+    const newUser = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+      },
+    });
+    res.status(200).json({
+      message: "create successfully",
+      data: newUser,
+    });
+  } catch (error) {
+    console.log("🚀 ~ file: userController.js:37 ~ createUser ~ error:", error);
+    res.status(400).json({
+      message: error.message,
+    });
+  }
 };
 
 const updateUser = (req, res, next) => {
